5ea7560e07de3a8c239f0485cc39968c8acfb125
JJayexd <kontakt.emilbj@gmail.com>
2025-09-15 11:40:32 +0200

    AuthProvider for Login

---
+++ Frontend/src/Providers/AuthProvider.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect, createContext, useContext } from "react";
+
+const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+    const [ loginData, setLoginData ] = useState('')
+
+    useEffect(() => {
+        if(sessionStorage.getItem('accessToken')) {
+            setLoginData(JSON.parse(sessionStorage.getItem('accessToken')))
+        }
+    }, [children]) 
+
+    return (
+        <AuthContext.Provider value={{ loginData, setLoginData }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+// Custom Hook
+export const useAuth = () => useContext(AuthContext)
