33f79b3d23a8fd9a98d0ff1bcff791a581139ba7
JJayexd <kontakt.emilbj@gmail.com>
2025-09-16 12:29:50 +0200

    ErrorBoundary

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,17 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AuthProvider } from '../src/Providers/AuthProvider.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from '../src/Components/ErrorFallback/ErrorFallback.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <ErrorFallback>
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          window.location.reload();
+        }}
+      >
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </ErrorFallback>
+      </ErrorBoundary>
     </AuthProvider>
   </StrictMode>,
 )
